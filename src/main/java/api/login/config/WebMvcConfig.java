2108b31a7086745cc4fa428e0ce69003412d8d77
Mun Kyoung-min <mun9659@naver.com>
2024-04-21 22:16:23 +0900

    Converter Init

---
+++ src/main/java/api/login/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package api.login.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        WebMvcConfigurer.super.addFormatters(registry);
+    }
+}
